513df05a8eeaa35980ee01fb6e3c6722940a97de
amjaad <amjad.alharbi@colorado.edu>
2017-03-11 23:29:52 -0700

    Implemented api request to get tweets

--- getTweets.py
+++ getTweets.py
@@ -3,35 +3,23 @@
 from urllib.request import Request
 import requests
 import config
-import json
-import time
 
-import time
-import CassandraManager
+class RequestWithMethod(Request):
+    def __init__(self, url, method, headers={}):
+        self._method = method
+        Request.__init__(self, url, headers)
 
+    def get_method(self):
+        if self._method:
+            return self._method
+        else:
+            return Request.get_method(self)
 
 if __name__ == "__main__":
     fromDate='201601010000'
     toDate='201701010000'
     query='lang=en'
-    PAUSE = 20 # seconds between page requests
-    maxResults="500"
     queryString = config.url + '?query=' + query+'&fromDate='+fromDate+'&toDate='+toDate
-    # +'&maxResults='+maxResults
-    next_token=''
-    numOftweets=1000
-    maxresults=int(maxResults)
-    count=numOftweets/maxresults
-    i=0
-    session = CassandraManager.createCassandraConnection();
-    while (next_token is not None) and i<count:
-        req = requests.get(queryString, auth=(config.username, config.password))
-        results=req.text
-        json_out = json.loads(results)
-        CassandraManager.insertIntoCassandra(json_out, session)
-        next_token = json_out['next']
-        queryString = config.url + '?query=' + query+'&fromDate='+fromDate+'&toDate='+toDate+'&next='+next_token
-        # +'&maxResults='maxResults
-        time.sleep(PAUSE)
-        # print(req.text)
-        i+=1
+    req = requests.get(queryString, auth=(config.username, config.password))
+    results=req.text
+    print(req.text)
