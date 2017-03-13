2c6bfd8b5d7e87719f73de5b2cc6a088187d3d3c
amjaad <amjad.alharbi@colorado.edu>
2017-03-12 20:26:03 -0600

    retrieve the next portion of the Tweets by passing next token

--- getTweets.py
+++ getTweets.py
@@ -3,7 +3,8 @@
 from urllib.request import Request
 import requests
 import config
-
+import json
+import time
 class RequestWithMethod(Request):
     def __init__(self, url, method, headers={}):
         self._method = method
@@ -19,7 +20,14 @@ if __name__ == "__main__":
     fromDate='201601010000'
     toDate='201701010000'
     query='lang=en'
+    PAUSE = 1 # seconds between page requests
     queryString = config.url + '?query=' + query+'&fromDate='+fromDate+'&toDate='+toDate
-    req = requests.get(queryString, auth=(config.username, config.password))
-    results=req.text
-    print(req.text)
+    next_token=''
+    while next_token is not None:
+        req = requests.get(queryString, auth=(config.username, config.password))
+        results=req.text
+        json_out = json.loads(results)
+        next_token = json_out['next']
+        queryString = config.url + '?query=' + query+'&fromDate='+fromDate+'&toDate='+toDate +'&next='+next_token
+        time.sleep(PAUSE)
+        print(req.text)
