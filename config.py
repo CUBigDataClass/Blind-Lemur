895d6aa94cf579fef1a1336780aa25a657c4e7ba
rakeshsm <rakeshsm91@gmail.com>
2017-03-18 03:23:47 -0600

    Adding Kafka consumer-producer

---
+++ config.py
@@ -0,0 +1,13 @@
+url= 'https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json'
+username = 'abu.sayed@colorado.edu'
+password = 'Blind-Lemur'
+
+# Follow Kafka and zookeeper installation steps here: https://kafka.apache.org/quickstart
+
+bootstrap_servers= "localhost:9092"
+kafka_topic = "kafkatopic"
+
+
+cassandra_keyspace= 'TwitterStorage'
+cassandra_createTableQuery= 'CREATE TABLE tweetTable (tweetID double PRIMARY KEY, tweetDate date, retweetCount int, tweet text);'
+
---
+++ getTweets.py
@@ -0,0 +1,66 @@
+
+from urllib.request import Request
+import requests
+import config
+import json
+import time
+#https://datastax.github.io/python-driver/installation.html
+from cassandra.cluster import Cluster
+from cassandra.query import BatchStatement, SimpleStatement
+import time
+from datetime import datetime
+from datetime import datetime, timedelta
+from email.utils import parsedate_tz
+
+def createCassandraConnection():
+    cluster = Cluster()
+    session = cluster.connect()
+    session.execute('USE '+config.cassandra_keyspace+';')
+    session.execute('DROP TABLE IF EXISTS TwitterStorage.tweetTable')
+    session.execute(config.cassandra_createTableQuery)
+    return session
+
+#return RFC822 compliant string date to UTC dae object
+def to_datetime(datestring):
+    time_tuple = parsedate_tz(datestring.strip())
+    dt = datetime(*time_tuple[:6])
+    return dt - timedelta(seconds=time_tuple[-1])
+
+def insertIntoCassandra(data, session):
+    for tweet in data["results"]:
+        tweet_date= to_datetime(tweet["created_at"])
+        tweet_id= tweet["id"]
+        tweet_text=tweet["text"]
+        retweet_count = tweet["retweet_count"]
+        results=[tweet_date,tweet_id,tweet_text,retweet_count]
+        batch = BatchStatement()
+        insert_data = session.prepare("INSERT INTO TwitterStorage.tweetTable (tweetID, tweetDate, retweetCount, tweet) VALUES (?, ?, ?, ?)")
+        batch.add(insert_data, (tweet_id, tweet_date, retweet_count, tweet_text))
+        session.execute(batch)
+
+
+if __name__ == "__main__":
+    fromDate='201601010000'
+    toDate='201701010000'
+    query='lang=en'
+    PAUSE = 20 # seconds between page requests
+    maxResults="500"
+    queryString = config.url + '?query=' + query+'&fromDate='+fromDate+'&toDate='+toDate
+    # +'&maxResults='+maxResults
+    next_token=''
+    numOftweets=1000
+    maxresults=int(maxResults)
+    count=numOftweets/maxresults
+    i=0
+    session = createCassandraConnection();
+    while (next_token is not None) and i<count:
+        req = requests.get(queryString, auth=(config.username, config.password))
+        results=req.text
+        json_out = json.loads(results)
+        insertIntoCassandra(json_out, session)
+        next_token = json_out['next']
+        queryString = config.url + '?query=' + query+'&fromDate='+fromDate+'&toDate='+toDate+'&next='+next_token
+        # +'&maxResults='maxResults
+        time.sleep(PAUSE)
+        # print(req.text)
+        i+=1
