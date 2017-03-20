cd27a7cf65e6710911244a4dcf647859b4291730
rakeshsm <rakeshsm91@gmail.com>
2017-03-19 19:13:56 -0600

    Adding Cassandra Manager

--- getTweets.py
+++ getTweets.py
@@ -5,18 +5,11 @@ import requests
 import config
 import json
 import time
-import re
-def parseJSON(data):
-    for tweet in data["results"]:
-        month= tweet["created_at"].split()[1]
-        tweet_id= tweet["id"]
-        tweet_text=tweet["text"]
-        text = re.sub(r"(?:\@|https?\://)\S+", "", tweet_text)
-        retweet_count = tweet["retweet_count"]
-        place= tweet["place"]
-        hashtags=tweet["entities"]["hashtags"]
-        results=[month,tweet_id,text,retweet_count,place,hashtags]
-        print(results)
+
+import time
+import CassandraManager
+
+
 if __name__ == "__main__":
     fromDate='201601010000'
     toDate='201701010000'
@@ -30,12 +23,12 @@ if __name__ == "__main__":
     maxresults=int(maxResults)
     count=numOftweets/maxresults
     i=0
-
+    session = CassandraManager.createCassandraConnection();
     while (next_token is not None) and i<count:
         req = requests.get(queryString, auth=(config.username, config.password))
         results=req.text
         json_out = json.loads(results)
-        parseJSON(json_out)
+        CassandraManager.insertIntoCassandra(json_out, session)
         next_token = json_out['next']
         queryString = config.url + '?query=' + query+'&fromDate='+fromDate+'&toDate='+toDate+'&next='+next_token
         # +'&maxResults='maxResults
