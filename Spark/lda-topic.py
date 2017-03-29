b8b236713e581e7eaa5af4c14b2ce3c4421ae914
amjaad <amjad.alharbi@colorado.edu>
2017-03-28 22:34:10 -0600

    filter tweets, tokenized each tweet and removed stop-words

--- Spark/lda-topic.py
+++ Spark/lda-topic.py
@@ -1,17 +1,41 @@
 from pyspark import SparkConf, SparkContext
-from textblob import TextBlob
+from string import printable
+from nltk.tokenize import RegexpTokenizer
+from langid import classify
+from stop_words import get_stop_words
 
 conf = SparkConf().setMaster("local").setAppName("LDA-topic")
 sc = SparkContext(conf = conf)
 
-def  isEnglish(tweet_text):
-    if(len(tweet_text)>2):
-        b=TextBlob(u""+tweet_text)
-        b.detect_language() =='en'
 
-#This should be replace by querying cassandra
+#isEnglish()) takes tweet_text(string) and then returns True if the string is English.
+def  isEnglish(tweet_text):
+    return classify(tweet_text)[0]=='en'
+def  unicode_to_str(u_str):
+    return u_str.encode('utf-8')
+def toprintable(text):
+    return  ''.join(filter(lambda x:x in printable, text))
+
+def tokenize(text):
+    tokenizer = RegexpTokenizer(r'\w+')
+    return tokenizer.tokenize(text.lower())
+def isStopWord(token):
+    en_stop = set(get_stop_words('en'))
+    return token not in en_stop
+
+#This should be replaced by querying cassandra
 tweets = sc.textFile("tweetsDB.txt")
 
 #cleaning dataset
 # 1) filter non-English tweets
-rdd = tweets.filter(isEnglish).collect()
+engTweets = tweets.filter(isEnglish).map(unicode_to_str)
+# 2) remove non-utf8 from the output of the above function
+cleanTweets=engTweets.map(toprintable)
+
+#3) Tokenization
+tokensRDD= cleanTweets.map(tokenize)
+#4) Remove stop_words
+tweets=tokensRDD.map(lambda TokenizedTweet:(filter(isStopWord,TokenizedTweet)))
+print(tweets.collect())
+#5) Remove digits & len(token)<2
+#6) Steaming
