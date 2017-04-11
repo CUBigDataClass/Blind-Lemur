3c77fe68bcec8da15160eb0bc448ae59320b2dbc
rakeshsm <rakeshsm91@gmail.com>
2017-04-11 16:15:11 -0600

    reading RDD from cassandra

--- Spark/sparkDb.py
+++ Spark/sparkDb.py
@@ -1,9 +1,21 @@
+import  config
 class sparkDb():
     def __init__(self):
         pass
 
     def readFromCassandra(self):
-        pass
+        from pyspark import SparkConf, SparkContext
+        from pyspark.sql.session import SparkSession
+        conf = SparkConf().setMaster("local").setAppName("testCassandra")
+
+        sc = SparkContext(conf=conf)
+
+        spark = SparkSession(sc)
+
+        rdd = spark.read.format("org.apache.spark.sql.cassandra").options(table=config.cassandra_table,
+                                                                          keyspace=config.cassandra_keyspace).load().rdd
+
+        return rdd
 
     def writeToCassandra(self):
         pass
