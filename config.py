4273bde634cc95c374c7881a080291c7081e7bb7
amjaad <amjad.alharbi@colorado.edu>
2017-04-22 14:47:34 -0600

    added config.py

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ username = 'abu.sayed@colorado.edu'
 password = ''
 
 
-cassandra_keyspace = 'TwitterStorage'
+cassandra_keyspace = 'twitterstorage'
 cassandra_table = 'tweettable'
-
+cassandra_IP= '34.208.209.148'
 cassandra_createTableQuery = 'CREATE TABLE IF NOT EXISTS twitterstorage.tweettable (tweetid double PRIMARY KEY,hashtags list<text>,retweetcount int,tweet text, tweetdate date)'
-
